46a80dedbed950db0c7fa5f2343bb86dc19e3b7d
alanef <alan@fullworks.net>
2021-07-22 16:48:34 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+This is the pre 3.0  Eventbrite widget markup and CSS for use only if you have custom CSS you don't want to change.
+
+Go to code  ( green button top right ) download zip and install as a plugin
---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+Plugin Name: Display Eventbrite Legacy Widget Template & CSS
+Plugin URI: https://github.com/alanef/display-eventbrite-template-legacy-widget
+Description: Legcay Widget as prior to v 3.0 of Display Eventbrite
+Version: 1.0
+Author: alan
+Author URI: https://fullworks.net
+License: GPL2
+
+No warranty, no unpaid support
+
+
+*/
+
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+add_filter( 'widget-for-eventbrite-api_template_paths', function ( $file_paths ) {
+	$file_paths[40] = trailingslashit( plugin_dir_path( __FILE__ ) ) . 'templates';
+
+	return $file_paths;
+}, 90 );
+
+add_action(
+	'wp_enqueue_scripts',
+	function () {
+		wp_enqueue_style( 'wfea-legacy-widget-style', plugin_dir_url( __FILE__ ) . 'css/style.css' );
+	}
+);
